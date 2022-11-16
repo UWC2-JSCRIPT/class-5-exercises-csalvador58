366c46d9c8f271984a2746a79c569a8dc25e618e
Chris Salvador <csalvador58@gmail.com>
2022-11-15 19:14:50 -0800

    Initial commit

--- accessing.js
+++ accessing.js
@@ -1,5 +1,8 @@
 // Change the text of the "Seattle Weather" header to "February 10 Weather Forecast, Seattle"
-
+const headerText = document.querySelector('#weather-head');
+headerText.innerText = 'February 10 Weather Forecast, Seattle';
 // Change the styling of every element with class "sun" to set the color to "orange"
+
+
 
 // Change the class of the second <li> from to "sun" to "cloudy"
